47b730d6043539fda31f5f3efe77c3b88b221aff
Ed Brown <teddybrown@ischool.berkeley.edu>
2023-11-28 10:22:21 -0800

    ported changes to resnet to validate, 2m on CPU to predict 2s on GPU to predict. TODO finish updating feature files

--- resnet101.py
+++ resnet101.py
@@ -1,8 +1,42 @@
+###############################################################################
+##
+## This file will live outside the feature creation pipeline since we don't
+## want to continually start and stop the model to predict against images.
+## This file will read an existing features.parquet file and add the VGG16
+## feature column to it.
+##
+## Batch load all images into numpy array
+## then predict!
+from io import BytesIO
+from pathlib import Path
+
 import numpy as np
+import polars as pl
+import tensorflow as tf
 from tensorflow.keras import Model
 from tensorflow.keras.applications.resnet import ResNet101, preprocess_input
 from tensorflow.keras.layers import Flatten, Input
-from tensorflow.keras.preprocessing import image
+
+# Read the parquet file, this takes a while. Leave it here
+train_features_file = Path("data/train_features.parquet")
+if not train_features_file.exists():
+    print("No train features file found. Please run the create_features_table first")
+    exit(1)
+
+test_features_file = Path("data/test_features.parquet")
+if not test_features_file.exists():
+    print("No train features file found. Please run the create_features_table first")
+    exit(1)
+
+dft = pl.read_parquet(test_features_file, use_pyarrow=True, memory_map=True)
+image_df = dft.select(["Image"])
+image_df = image_df.with_columns(
+    pl.col("Image").map_elements(lambda x: np.load(BytesIO(x))).alias("NumPy")
+)
+input_images = np.asarray(image_df["NumPy"].to_list())
+preprocessed_data = preprocess_input(input_images)
+dataset = tf.data.Dataset.from_tensor_slices(preprocessed_data)
+dataset = dataset.batch(100)
 
 # Change input shape
 input_shape = (64, 64, 3)  # Assuming 3 channels for RGB images
@@ -16,25 +50,7 @@ model = Model(inputs=resnet101.input, outputs=flatten_layer)
 for layer in model.layers:
     layer.trainable = False
 
+embeddings = model.predict(dataset)
 
-def preprocess_image(img_path):
-    img = image.load_img(img_path, target_size=(64, 64))
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = preprocess_input(img_array)
-    return img_array
-
-
-def get_resnet_embeddings(image_path):
-    img_array = preprocess_image(img_path)
-    embeddings = model.predict(img_array)
-    return np.squeeze(embeddings)  # remove the extra dimension
-
-
-img_path = "sign_data/Test/00000.png"
-embeddings = get_resnet_embeddings(img_path)
-
-print(model.summary())
-np.save("resnet_embeddings.npy", embeddings)
 print(embeddings)
 print(embeddings.shape)
